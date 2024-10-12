e69d382e09c2f9838804f933fbf378f46e4dbee6
Arjun Wadurkar <arjunwadurkar28@gmail.com>
2024-10-12 18:19:22 +0530

    assignment-1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TaskFormComponent } from 'src/Components/task-form/task-form.component';
+import { TaskListComponent } from 'src/Components/task-list/task-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {
+  //   path :'',
+  //   component : TaskListComponent,
+  // },
+  {
+    path : '',
+    component : TaskListComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
